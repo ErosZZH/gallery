3818a6e17a89b9441c8fc7e89519782fb0e23189
eroszzh <zhenhzhu@126.com>
2016-10-21 13:34:47 +0800

    add upload page

--- client/containers/upload/index.js
+++ client/containers/upload/index.js
@@ -4,16 +4,31 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+require('./styles.scss');
+
 export class Upload extends Component {
+  static propTypes = {
+    imageDatas: PropTypes.array
+  };
+
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <div>This is upload page.</div>
+      <section>
+        <h1>上传图片</h1>
+        <section className="container">
+          {
+            this.props.imageDatas.map((imageData, index) =>
+              <img className="img-editor" key={index} src={imageData.imageURL} />
+            )
+          }
+        </section>
+      </section>
     );
   }
 }
 
-export default connect()(Upload);
+export default connect(state => {return {imageDatas: state.image.imageDatas}})(Upload);
